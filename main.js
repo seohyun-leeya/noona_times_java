0669b91a7d281f4367269f986e337030e8c2e10e
Seohyun Lee <seohyun2056@gmail.com>
2025-02-27 00:06:23 +0900

    링크수정

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY=`bd04bad3a7ba4d34bb6e9f11332ae63e`
 let news = []
 
 const getLatestNews = async() => {
-    const url = new URL (`https://study-website-be-bbb1539aa813.herokuapp.com/`
+    const url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/`
     )
     const response = await fetch (url)
     const data = await response.json ()
