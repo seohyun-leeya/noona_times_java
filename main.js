8c42959d935f013011cc258500c033f7c590eac6
Seohyun Lee <seohyun2056@gmail.com>
2025-02-27 12:12:08 +0900

    호출 주소 수정

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-const API_KEY=`bd04bad3a7ba4d34bb6e9f11332ae63e`
+// const API_KEY=`bd04bad3a7ba4d34bb6e9f11332ae63e`
 let newsList = []
 
 const getLatestNews = async() => {
-    const url = new URL (`https://noona-times-be-5ca9402f90d9.herokuapp.com/`
-    )
+    let url =new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
     const response = await fetch (url)
     const data = await response.json ()
     newsList = data.articles
